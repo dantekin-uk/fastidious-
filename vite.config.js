ab3a54f525bc879ce2a94d1e15e1f9531b00bb5d
unknown <your.email@example.com>
2026-02-10 14:00:52 +0300

    fix: update asset paths and Vite configuration for production

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command, mode }) => {
-  // Load env file based on `mode` in the current working directory.
-  // Set the third parameter to '' to load all env variables regardless of the `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
-  
-  return {
-    base: env.NODE_ENV === 'production' ? '/fastidious-/' : '/',
-    plugins: [react()],
-    server: {
-      port: 3000,
-      host: true
-    },
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets',
-      emptyOutDir: true,
-    }
+export default defineConfig({
+  plugins: [react()],
+  base: '/fastidious-/',
+  server: {
+    port: 3000,
+    host: true
   }
 })
