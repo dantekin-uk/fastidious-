ee48d673d74a4058d3a74f66ddd0153bcd49b3ab
unknown <your.email@example.com>
2026-02-10 14:09:47 +0300

    fix: update asset paths and configuration for production build

--- vite.config.js
+++ vite.config.js
@@ -14,21 +14,10 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        assetFileNames: (assetInfo) => {
-          let extType = assetInfo.name.split('.').at(1);
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = 'images';
-          }
-          return `assets/${extType}/[name]-[hash][extname]`;
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
       },
-    },
-  },
-  css: {
-    modules: {
-      generateScopedName: '[name]__[local]___[hash:base64:5]',
     },
   },
 })
